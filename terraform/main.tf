d17236549401d55342154091a784ad80ffa11f2c
nattyr <nathan-r@hotmail.com.au>
2024-08-11 17:30:34 +1000

    Changed resume s3 bucket name (needs to be unique)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "cloud-resume-website" {
-    bucket = "cloud-resume-website"
+    bucket = "cloud-resume-website-nr"
     tags = {
-        Name = "cloud-resume-website"
+        Name = "cloud-resume-website-nr"
         }
 }
