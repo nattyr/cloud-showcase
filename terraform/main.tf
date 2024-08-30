4e7061cb9ca55380049d41f9c9030484e2c45303
nattyr <nathan-r@hotmail.com.au>
2024-08-30 19:54:07 +1000

    Added Cloudfront Distribution

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,66 @@
 resource "aws_s3_bucket" "cloud-resume-website" {
-    bucket = "cloud-resume-website-nr"
-    tags = {
-        Name = "cloud-resume-website-nr"
+  bucket = "cloud-resume-website-nr"
+  tags = {
+    Name = "cloud-resume-website-nr"
+    }
+}
+
+resource "aws_cloudfront_distribution" "resume_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.cloud-resume-website.bucket_regional_domain_name
+    origin_id   = "S3-resume-bucket"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-resume-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "resume_bucket_policy" {
+  bucket = aws_s3_bucket.resume_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.resume_bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "${aws_cloudfront_distribution.resume_distribution.arn}"
+          }
         }
+      }
+    ]
+  })
 }
