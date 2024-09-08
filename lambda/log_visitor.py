a5cf2bbef1600097e922edac2a26e4fd0cedf5a9
nattyr <nathan-r@hotmail.com.au>
2024-09-08 21:44:37 +1000

    Updated lambda python script

--- lambda/log_visitor.py
+++ lambda/log_visitor.py
@@ -1,5 +1,25 @@
+import json
+import boto3
+import os
+from datetime import datetime
+import pytz
+
+def get_aet_time():
+    utc_time = datetime.now(pytz.utc)
+    aet = pytz.timezone('Australia/Sydney')
+    aet_time = utc_time.astimezone(aet)
+    aet_iso = aet_time.isoformat()
+    return aet_iso
+
 def hit(event, context):
+    ip_address = event['requestContext']['identity']['sourceIp']
+    current_aet_datetime = get_aet_time()
+
     return {
         'statusCode': 200,
-        'body': '10'
+        # 'body': '10'
+        'body': json.dumps({
+            'ip_address': ip_address,
+            'current_aet_datetime': current_aet_datetime
+        })
     }
