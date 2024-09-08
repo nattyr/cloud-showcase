6e8de1338fa8d33975869edbc1627234b58c4845
nattyr <nathan-r@hotmail.com.au>
2024-09-08 17:46:29 +1000

    Added API gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -233,3 +233,36 @@ resource "aws_lambda_function" "log_visitor_lambda" {
     }
   }
 }
+
+resource "aws_api_gateway_rest_api" "visitor" {
+  name = "VisitorAPI"
+}
+
+resource "aws_api_gateway_resource" "my_resource" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  parent_id = aws_api_gateway_rest_api.visitor.root_resource_id
+  path_part = "hit"
+}
+
+resource "aws_api_gateway_method" "visitor_get" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "visitor_lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.visitor_get.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.log_visitor_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.visitor.execution_arn}/*/*"
+}
