4cecdb446e2352e18ddf2be08b3482720bd92a8a
nattyr <nathan-r@hotmail.com.au>
2024-09-11 21:53:17 +1000

    Completed log_visitor lambda function

--- lambda/log_visitor.py
+++ lambda/log_visitor.py
@@ -1,25 +1,78 @@
-import json
 import boto3
 import os
+import dateutil.tz
+import json
 from datetime import datetime
-import pytz
 
-def get_aet_time():
-    utc_time = datetime.now(pytz.utc)
-    aet = pytz.timezone('Australia/Sydney')
-    aet_time = utc_time.astimezone(aet)
-    aet_iso = aet_time.isoformat()
-    return aet_iso
+dynamodb = boto3.resource('dynamodb')
+log_table_name = os.environ.get('LOG_TABLE_NAME')
+count_table_name = os.environ.get('COUNT_TABLE_NAME')
+
+def log_hit(ip, log_datetime):
+    try:
+        table = dynamodb.Table(log_table_name)
+        response = table.put_item(
+            Item={
+                'IPAddress': ip,
+                'DateTime': log_datetime
+            }
+        )
+        return response
+    
+    except Exception as e:
+        print(f"Error logging hit: {e}")
+        return None
+    
+def increment_hit_counter():
+    try:
+        table = dynamodb.Table(count_table_name)
+        
+        # Todo: implement dynamicaly fetched page title
+        page_name = "home"
+        
+        # If the record does not exist, update_item will create it
+        update_response = table.update_item(
+            Key={'PageName': page_name},
+            UpdateExpression='SET #count = if_not_exists(#count, :start) + :increment',
+            ExpressionAttributeNames={'#count': 'count'},
+            ExpressionAttributeValues={
+                ':start': 0,
+                ':increment': 1
+                },
+            ReturnValues='ALL_NEW'
+        )
+
+        return update_response
+    
+    except Exception as e:
+        print(f"Error incrementing hit counter: {e}")
+        return None
 
 def hit(event, context):
-    ip_address = event['requestContext']['identity']['sourceIp']
-    current_aet_datetime = get_aet_time()
+    try:
+        # Get source IP address
+        ip_address = event['requestContext']['identity']['sourceIp']
+        
+        # Get current time in AET
+        aet = dateutil.tz.gettz('Australia/Sydney')
+        current_datetime_aet = datetime.now(tz=aet).isoformat()
+        
+        log_response = log_hit(ip_address, current_datetime_aet)
+        increment_response = increment_hit_counter()
 
-    return {
-        'statusCode': 200,
-        # 'body': '10'
-        'body': json.dumps({
-            'ip_address': ip_address,
-            'current_aet_datetime': current_aet_datetime
-        })
-    }
+        # Get hit count
+        count = "0"
+        if 'Attributes' in increment_response:
+                count = increment_response.get('Attributes').get('count')
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'count': str(count)})
+        }
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal Server Error'})
+        }
