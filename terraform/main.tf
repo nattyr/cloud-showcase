c381554d1b13858d7afd83ec47149cc61415e666
nattyr <nathan-r@hotmail.com.au>
2024-09-12 21:47:37 +1000

    Added CORS to hit endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -313,3 +313,50 @@ resource "aws_api_gateway_usage_plan_key" "visitor_throttle" {
   key_type      = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.visitor.id
 }
+
+resource "aws_api_gateway_method" "visitor_options" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor.id
+  resource_id   = aws_api_gateway_resource.my_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "visitor_options" {
+  rest_api_id = aws_api_gateway_method.visitor_options.rest_api_id
+  resource_id = aws_api_gateway_method.visitor_options.resource_id
+  http_method = aws_api_gateway_method.visitor_options.http_method
+  type        = "MOCK"
+
+  integration_http_method = "OPTIONS"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+}
+
+resource "aws_api_gateway_integration_response" "visitor_options" {
+  rest_api_id = aws_api_gateway_integration.visitor_options.rest_api_id
+  resource_id = aws_api_gateway_integration.visitor_options.resource_id
+  http_method = aws_api_gateway_integration.visitor_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'X-Api-Key'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'nathanrichardson.dev'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "visitor_options" {
+  rest_api_id = aws_api_gateway_integration_response.visitor_options.rest_api_id
+  resource_id = aws_api_gateway_integration_response.visitor_options.resource_id
+  http_method = aws_api_gateway_integration_response.visitor_options.http_method
+  status_code = aws_api_gateway_integration_response.visitor_options.status_code
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
