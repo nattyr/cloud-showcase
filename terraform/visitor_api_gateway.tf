1ee90462e5a45a44ea812ec60b9db2e29ee86401
nattyr <nathan-r@hotmail.com.au>
2024-09-14 16:25:00 +1000

    Moved api gateway to new TF file

---
+++ terraform/visitor_api_gateway.tf
@@ -0,0 +1,141 @@
+resource "aws_api_gateway_rest_api" "visitor" {
+  name = "VisitorAPI"
+}
+
+resource "aws_api_gateway_resource" "my_resource" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  parent_id = aws_api_gateway_rest_api.visitor.root_resource_id
+  path_part = "hit"
+}
+
+resource "aws_api_gateway_method" "visitor_get" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = "GET"
+  authorization = "NONE"
+  api_key_required = true
+}
+
+resource "aws_api_gateway_integration" "visitor_lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.visitor_get.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.log_visitor_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.log_visitor_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.visitor.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "visitor_prod" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_api_gateway_method.visitor_get]
+}
+
+resource "aws_api_gateway_api_key" "visitor" {
+  name = "VisitorApiKey"
+  description = "API key with rate throttling"
+  enabled = true
+}
+
+resource "aws_api_gateway_stage" "visitor_prod" {
+  stage_name = "prod"
+  deployment_id = aws_api_gateway_deployment.visitor_prod.id
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+}
+
+resource "aws_api_gateway_usage_plan" "visitor" {
+  name = "VisitorUsagePlan"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.visitor.id
+    stage = aws_api_gateway_stage.visitor_prod.stage_name
+  }
+
+  throttle_settings {
+    burst_limit = 7
+    rate_limit  = 3
+  }
+
+  quota_settings {
+    limit  = 200
+    period = "DAY"
+  }
+
+  depends_on = [aws_api_gateway_stage.visitor_prod]
+}
+
+resource "aws_api_gateway_usage_plan_key" "visitor_throttle" {
+  key_id = aws_api_gateway_api_key.visitor.id
+  key_type = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.visitor.id
+}
+
+resource "aws_api_gateway_method" "visitor_options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = "OPTIONS"
+  authorization = "NONE"
+  api_key_required = false
+}
+
+resource "aws_api_gateway_integration" "visitor_options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.visitor_options.http_method
+  type = "MOCK"
+  integration_http_method = "OPTIONS"
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates = {
+    "application/json" = <<EOF
+    {
+      "statusCode": 200
+    }
+    EOF
+  }
+  
+}
+
+resource "aws_api_gateway_method_response" "visitor_options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.visitor_options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "visitor_options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.visitor_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'X-Api-Key'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://nathanrichardson.dev'"
+  }
+
+  depends_on = [aws_api_gateway_integration.visitor_options]
+}
